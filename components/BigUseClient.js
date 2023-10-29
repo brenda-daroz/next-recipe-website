ecbc3fe01c9c131faab2da9a48c69ea1103d2680
Brenda Daroz <brenda.daroz@gmail.com>
2023-10-29 18:04:05 +0100

    format file

--- components/BigUseClient.js
+++ components/BigUseClient.js
@@ -1,8 +1,6 @@
-// "use client";
-// import PrefixedLink from './PrefixedLink.js'
-import capitalizeFirstLetter from '../utils/capitalizeFirstLetter.js'
-import styled from 'styled-components'
-import Link from 'next/link.js';
+import capitalizeFirstLetter from "../utils/capitalizeFirstLetter.js";
+import styled from "styled-components";
+import Link from "next/link.js";
 
 const ListContainer = styled.div`
   margin-top: 20px;
@@ -14,32 +12,34 @@ const CategoryTitle = styled.h2`
 `;
 
 const RecipeLink = styled.div`
- display: inline-block;
+  display: inline-block;
   font-size: 1.2rem;
   color: #333;
   text-decoration: none;
   margin-bottom: 5px;
-
 
   &:hover {
     color: #ff5733;
   }
 `;
 
-export default function BigUseClient({ data }) {
-
-  const savory = data.recipes.filter(recipe => recipe.type === "savory")
-  const sweet = data.recipes.filter(recipe => recipe.type === "sweet")
-
+export default function BigUseClient({ recipes }) {
+  const savory = recipes.filter((recipe) => recipe.type === "savory");
+  const sweet = recipes.filter((recipe) => recipe.type === "sweet");
 
   const RecipeList = ({ recipes, category }) => (
     <ListContainer>
       <CategoryTitle>{capitalizeFirstLetter(category)}</CategoryTitle>
       <ul>
         {recipes.map((recipe) => (
-          <li
-          key={recipe.id}>
-            <Link href={{pathname:`/recipes/${encodeURIComponent(recipe.id)}`, query: { object: JSON.stringify(recipe) }}} as={`/recipes/${recipe.id}`}>
+          <li key={recipe.id}>
+            <Link
+              href={{
+                pathname: `/recipes/${encodeURIComponent(recipe.id)}`,
+                query: { object: JSON.stringify(recipe) },
+              }}
+              as={`/recipes/${recipe.id}`}
+            >
               <RecipeLink>{capitalizeFirstLetter(recipe.title)}</RecipeLink>
             </Link>
           </li>
@@ -55,6 +55,4 @@ export default function BigUseClient({ data }) {
       <RecipeList recipes={sweet} category="sweet" />
     </>
   );
-
-
 }
