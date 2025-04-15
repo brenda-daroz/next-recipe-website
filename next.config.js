73932bb1e5d327db115b6de2e915e7c31bc52db3
bdaroz <bdaroz@tesla.com>
2025-04-15 20:44:58 +0200

    remove basePath

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const pathPrefix = process.env.NODE_ENV === 'production'
-  ? '/next-recipe-website'
-  : '';
-
 module.exports = {
-  basePath: pathPrefix,
-  output: "export",
   compiler: {
     styledComponents: true,
   }
