f8e84f787a96fdb230882464e94f11569d145770
Brenda Daroz <brenda.daroz@gmail.com>
2025-08-19 11:08:46 +0200

    test

--- app/lib/context/SessionContext.tsx
+++ app/lib/context/SessionContext.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import React from 'react';
 import { createContext, useContext, useEffect, useState } from "react";
 
 type User = { id: string; name: string; email: string; role: string };
